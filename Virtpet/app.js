4e34a2e220b645a455651ad00f92e54325e23dbb
Markus Owens <167331008+27MOwens@users.noreply.github.com>
2026-01-14 09:59:45 -0500

    store buy

--- Virtpet/app.js
+++ Virtpet/app.js
@@ -179,12 +179,32 @@ app.post('/store', (req,res) => {
    if (buyresponse) {
        //process buy
        logger.info(`Processing buy of item ${buyresponse} for user ${req.session.user}`);
-   }
-    if (sellresponse) {
+       let uid = req.body.uid;
+       if (buyresponse === '1') { dbp.run('INSERT OR IGNORE INTO playerinv (iid,itemS1) VALUES (?, ?)', [uid,buyresponse], function (err) {
+            if (err) {
+                logger.error(err.message);
+            }
+            });
+            dbp.run('UPDATE playerinv SET money = money - 1 WHERE iid = ?', [uid], function (err) {
+                if (err) {
+                    logger.error(err.message);
+                }
+            });
+            dbp.run('UPDATE storeInv SET money = money + 1 WHERE sid = ?', [1], function (err) {
+                if (err) {
+                    logger.error(err.message);
+                }
+            }); 
+            
+        }
+       else if (buyresponse === '2') { }
+       else if (buyresponse === '3') { }
+       else { logger.warn(`Invalid buy response(numbers or no spaces): ${buyresponse}`); }
+   
+   if (sellresponse) {
         //process sell
         logger.info(`Processing sell of item ${sellresponse} for user ${req.session.user}`);
-    }
-});
+    }}});
 //socket.io setup
 io.on('connection', (socket) => {
     logger.info('a user connected');
